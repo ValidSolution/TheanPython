642d6b16c3ec336b968d25c5bab7e5bb7e8325ca
huangan <huangan@laiye.com>
2019-10-08 15:53:30 +0800

    组织目录

--- src/com/thean/python/tf/test_main.py
+++ src/com/thean/python/tf/test_main.py
@@ -1,4 +1,4 @@
-from venv.com.thean.python.tf import input_data
+from src.com.thean.python.tf import input_data
 import tensorflow as tf
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
