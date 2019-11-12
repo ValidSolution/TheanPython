1bdfb08640fd443b2daf181cd7d35fe5a9b4581e
huangan <huangan@laiye.com>
2019-11-12 20:21:52 +0800

    图像处理常用方法

--- src/com/thean/python/pic.py
+++ src/com/thean/python/pic.py
@@ -1,4 +1,6 @@
 from PIL import Image
+from PIL import ImageFilter
+from PIL import ImageEnhance
 import numpy as np
 
 
@@ -28,6 +30,82 @@ def hand_draw(input_path, output_path):
     im = Image.fromarray(gd.astype('uint8'))  # 重构图像
     im.save(output_path)  # 保存图像
 
+
+def pic_dealer_example(pic_path, output_path):
+    im = Image.open(pic_path)
+    # 创建缩略图
+    im.thumbnail((128, 128), Image.ANTIALIAS)
+    im.save(output_path + "/thumbnail.jpg")
+    im = Image.open(pic_path)
+    # 剪切图片，四元组分别表示左、上、右、下（以图片左上角为坐标原点）
+    # 示例代码剪切了一块 200x200 pixel的区域
+    region = im.crop((100, 100, 300, 300))
+    region.save(output_path + "/crop.jpg")
+    # 图片粘贴，第二个参数含义同上
+    region = region.transpose(Image.ROTATE_180)
+    im.paste(region, (100, 100, 300, 300))
+    im.save(output_path + "/paste.jpg")
+    im = Image.open(pic_path)
+    # 分离和合并颜色通道
+    r, g, b = im.split()
+    im2 = Image.merge("RGB", (r, g, b))
+    im2.save(output_path + "/merge.jpg")
+    # 对图像进行几何变换是一种基本处理，在Pillow中包括resize()和rotate()，如用法如下：
+    # 调整图片大小
+    out = im.resize((128, 128))
+    # 旋转图片，参数为顺时针旋转（degree conter-clockwise）的角度，或者用Image定义好的值
+    out = im.rotate(45)
+    out = im.transpose(Image.FLIP_LEFT_RIGHT)
+    out = im.transpose(Image.FLIP_TOP_BOTTOM)
+    out = im.transpose(Image.ROTATE_90)
+    out = im.transpose(Image.ROTATE_180)
+    out = im.transpose(Image.ROTATE_270)
+    out.save(output_path + "/rotate.jpg")
+    # 进行颜色空间的变换
+    cmyk = im.convert("CMYK")
+    cmyk.save(output_path + "/cmyk.jpg")
+    gray = im.convert("L")
+    gray.save(output_path + "/gray.jpg")
+    '''
+    图像滤波
+    
+    '''
+    imgF = Image.open(pic_path)
+    # 均值滤波（模糊），图片看起来柔和一些
+    imgF.filter(ImageFilter.BLUR).save(output_path + "/blur.jpg")
+    # 图片细节效果，没看出什么区别
+    imgF.filter(ImageFilter.DETAIL).save(output_path + "/detail.jpg")
+    # 轮廓效果
+    imgF.filter(ImageFilter.CONTOUR).save(output_path + "/contour.jpg")
+    # 边界效果
+    imgF.filter(ImageFilter.FIND_EDGES).save(output_path + "/edges.jpg")
+    imgF.filter(ImageFilter.EDGE_ENHANCE).save(output_path + "/edge_enhance.jpg")
+    # 阈值边界加强
+    imgF.filter(ImageFilter.EDGE_ENHANCE_MORE).save(output_path + "/edge_enhance_more.jpg")
+    # 浮雕
+    imgF.filter(ImageFilter.EMBOSS).save(output_path + "/emboss.jpg")
+    # 平滑，柔和效果貌似比均值滤波好些
+    imgF.filter(ImageFilter.SMOOTH).save(output_path + "/smooth.jpg")
+    # 阈值平滑
+    imgF.filter(ImageFilter.SMOOTH_MORE).save(output_path + "/smooth_more.jpg")
+    # 锐化
+    imgF.filter(ImageFilter.SHARPEN).save(output_path + "/sharpen.jpg")
+    '''
+    图像增强
+    '''
+    imgE = Image.open(pic_path)
+    # 增强对比度 0.0（solid grey实灰）~1.0（原图）
+    imgEH = ImageEnhance.Contrast(imgE)
+    imgEH.enhance(1.3).show()
+    # 色彩平衡 0.0（黑白）~1.0（原图）
+    imgEH = ImageEnhance.Color(imgE)
+    imgEH.enhance(0.5).show()
+    # 亮度 0.0（黑）~1.0（原图）
+    imgEH = ImageEnhance.Brightness(imgE)
+    imgEH.enhance(0.5).show()
+    # 锐化 0.0（模糊）~1.0（原图）~2.0（尖锐）
+    imgEH = ImageEnhance.Sharpness(imgE)
+    imgEH.enhance(1.5).show()
 
 if __name__ == "__main__":
     hand_draw("/path/to/your/pic.jpg", "/path/to/save/this/pic.jpg")
